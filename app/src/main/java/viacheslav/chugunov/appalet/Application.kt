ac8534bf48145ba27f8ffb3b04b209b26364618e
viacheslav-chugunov <68062984+viacheslav-chugunov@users.noreply.github.com>
2022-04-15 17:45:16 +0300

    Added firebase analytics

--- app/src/main/java/viacheslav/chugunov/appalet/Application.kt
+++ app/src/main/java/viacheslav/chugunov/appalet/Application.kt
@@ -1,7 +1,15 @@
 package viacheslav.chugunov.appalet
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class Application : Application()
+class Application : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+    }
+
+}
