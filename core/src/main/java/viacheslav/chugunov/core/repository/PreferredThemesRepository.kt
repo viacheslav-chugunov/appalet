c4105f9c3b1b5f977c6cb63152e78e733444fe1d
viacheslav-chugunov <68062984+viacheslav-chugunov@users.noreply.github.com>
2022-04-16 18:45:46 +0300

    Extended PreferredThemesRepository

--- core/src/main/java/viacheslav/chugunov/core/repository/PreferredThemesRepository.kt
+++ core/src/main/java/viacheslav/chugunov/core/repository/PreferredThemesRepository.kt
@@ -1,10 +1,12 @@
 package viacheslav.chugunov.core.repository
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import viacheslav.chugunov.core.model.Theme
 
 interface PreferredThemesRepository {
     fun getThemesFlow(): Flow<List<Theme>>
+    suspend fun getThemes(): List<Theme> = getThemesFlow().first()
     suspend fun addTheme(theme: Theme)
     suspend fun removeTheme(theme: Theme)
     suspend fun isThemeAdded(theme: Theme): Boolean
